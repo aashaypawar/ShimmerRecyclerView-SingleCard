12be63504b72fad7c23a577a110abb47e6debb90
Aashay <Aashay@Aashays-MacBook-Air.local>
2020-09-16 15:53:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shimmerrecyclerview/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.shimmerrecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cooltechworks.views.shimmer.ShimmerRecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
